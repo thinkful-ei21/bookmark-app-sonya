ef14112d93903a5170e2f3e574c26d1c83347ff6
Sonya Slegers <sonjay08@gmail.com>
2018-05-17 19:29:18 -0500

    added api and user input error handling functions

--- scripts/bookmark-list.js
+++ scripts/bookmark-list.js
@@ -2,32 +2,42 @@
 /* global api store */
 
 const bookmarkList = (function() {
-
+  //translate api error message to be more user friendly
   const generateBookmarkElement = function() {
     //html template code will be returned
   };
 
+  const formValidation = function() {
+    return ($('.js-title-entry').val() && $('.js-url-entry').val());
+  };
+
   const handleNewBookmarkSubmit = function() {
-   
    
     $('#js-add-item-form').submit(function(event) {
       event.preventDefault();
-      const formData = new FormData(this);
-      const formDataObject = {};
-      formData.forEach(function(value, name) {
-        formDataObject[name] = value;
-      });
-      api.createBookmark(formDataObject, 
-        (newBookmark) => {
-          store.addItems(newBookmark);
+      
+      if (!formValidation(this)) {
+        $('.error-container').html('Please enter a valid Title and a valid Web Address');
+      } else {
+        $('.error-container').html('');
+        const formData = new FormData(this);
+        const formDataObject = {};
+        formData.forEach(function(value, name) {
+          formDataObject[name] = value;
+        });
+        api.createBookmark(formDataObject, 
+          (newBookmark) => {
+            store.addItems(newBookmark);
           //render();
-        },
-        (err) => {
-           console.log(err);
-           store.setError(err);
+          },
+          (err) => {
+            ('.error-container').html('There seems to be a problem with the server.  Please try again.');
+            console.log(err);
+            store.setError(err);
           //render();
-        }
-      );
+          }
+        );
+      }
     });
   };
 
